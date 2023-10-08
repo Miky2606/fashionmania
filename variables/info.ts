b7037ea1a4e1b3076bd6f4ef49345c97ea8e7d26
Jonathan <jonathanjgn99@gmail.com>
2023-10-08 14:02:08 -0400

    first commit

---
+++ variables/info.ts
@@ -0,0 +1,29 @@
+export interface InfoTop {
+  name: string;
+  image: string;
+  id: number;
+  text: string;
+}
+
+export const infoVar: InfoTop[] = [
+  {
+    id: 3,
+    name: "Jennifer Lawrence",
+    image:
+      "https://resources.diariolibre.com/images/binrepository/jennifer_15727341_20210208120604.jpg",
+    text: "Jennifer Lawrence no podia quedarse fuera de esta lista, la actriz que dio vida a Mystic en X-Men se convirtio en un referente femenino tanto dentro como fuera de la industria de Hollywood. Es una de las actrices mas destacadas de la epoca actual, ademas de ser una mujer realmente hermosa.",
+  },
+  {
+    id: 2,
+    name: "Amber Heard",
+    image:
+      "https://resources.diariolibre.com/images/binrepository/amber_15727342_20210208120604.jpg",
+    text: "Ella es, segun el estudio, la segunda mujer mas hermosa de las mujeres mas hermosas segun la ciencia. Amber Head no solo es bellisima, sino que su carrera interpretativa la hicieron despegar como una de las actrices mas comprometidas de los tiempos que corren.",
+  },
+  {
+    id: 1,
+    name: "Melani Boza",
+    image: "../images/m1.jpg",
+    text: "No hay nadie mas hermoso en este mundo que esta mujer, segun muchos expertos en belleza dicen que nunca habia visto a alguien tan hermosa como ella. Otras personas dicen que debido a su sonrisa, hace la vida mas bella. Ella es  una belleza perfecta, no hay nada que necesite sumar o restar. Pero yo digo que los destellos del atardecer son muy hermosos, pero no pueden compararse con el brillo de sus ojos.",
+  },
+];
