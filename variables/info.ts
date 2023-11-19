efd3864ec0f2b9569bbff41043267be2184a4902
Jonathan <jonathanjgn99@gmail.com>
2023-11-19 13:53:23 -0500

    New Update

--- variables/info.ts
+++ variables/info.ts
@@ -22,8 +22,9 @@ export const infoVar: InfoTop[] = [
   },
   {
     id: 1,
-    name: "Ibied Herrera ",
-    image: "../images/m1.jpg",
+    name: "Natali Tejeda",
+    image:
+      "https://scontent-mia3-2.xx.fbcdn.net/v/t39.30808-6/356418253_956662692239766_7257375742583812861_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YOZVydmyRwgAX9f67cL&_nc_ht=scontent-mia3-2.xx&oh=00_AfC9LD1VzwmpozDf88kBIWmvmAx5Yn6b4oQ6CS38bVqtwQ&oe=655FCFC5",
     text: "No hay nadie mas hermoso en este mundo que esta mujer, segun muchos expertos en belleza dicen que nunca habia visto a alguien tan hermosa como ella. Otras personas dicen que debido a su sonrisa, hace la vida mas bella. Ella es  una belleza perfecta, no hay nada que necesite sumar o restar. Pero yo digo que los destellos del atardecer son muy hermosos, pero no pueden compararse con el brillo de sus ojos.",
   },
 ];
